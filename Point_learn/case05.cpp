8eed50dea4f6dce859364cd7caadaee90263a66c
roohom <roohom@qq.com>
2019-01-08 23:51:34 +0800

    some practices(from MiBook Pro 15.6")

--- Point_learn/case05.cpp
+++ Point_learn/case05.cpp
@@ -11,30 +11,51 @@
 typedef struct LNode{
     int data;
     struct LNode *next;
-}LNnode;
+}LNode, *List;
 
-
+/*用这种输出方式为什么不行？只打印出一串内存地址？？？
 //输出链表的元素
-void DispList(LNode *L)            //输出链表
+int dispList(LNode *L)            //输出链表
 {
     LNode *i;
-    i = L->next;
-    if (i==NULL) return ;
+    i = L;
+    if (i->next== nullptr) return 0;
     do
     {
-        printf("%d", L->data);
-        i = L->next;
+        printf("%d", i->data);
+        i = i->next;
     }
-    while(i==NULL);
+    while(i == nullptr);
     printf("\n");
+}
+用这种输出方式为什么不行？只打印出一串内存地址？？？*/
+
+int DisplayList(List l)
+{
+    List p = nullptr;
+    p = l->next;
+    int k = 0;
+    while (p)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+        k++;
+    }
+    if (k == 0)
+    {
+        printf("链表为空");
+        return false;
+    }
+    printf("\n");
+    return 1;
 }
 
 //尾插法建立链表C
-void tailCreatList(LNnode *&C, int a[], int n){
-    LNnode *s,*r;                          //s用来指向新申请的节点，始终指向终端节点
+void tailCreatList(LNode *&C, int a[], int n){
+    LNode *s,*r;                          //s用来指向新申请的节点，始终指向终端节点
     int i;
-    C = (LNnode *)malloc(sizeof(LNode));   //申请内存空间建立链表C
-    C->next = NULL;                        //表示链表为空
+    C = (LNode *)malloc(sizeof(LNode));   //申请内存空间建立链表C
+    C->next = nullptr;                        //表示链表为空
     r = C;                                 //r始终指向终端节点
     for(i = 0; i<n; ++i)
     {
@@ -43,7 +64,8 @@ void tailCreatList(LNnode *&C, int a[], int n){
         r->next = s;
         r = r->next;
     }
-    r->next = NULL;                         //数组a中所有的元素都已经装入链表中，现在将链表的末尾指针域置为NULL
+    r->next = nullptr;                         //数组a中所有的元素都已经装入链表中，现在将链表的末尾指针域置为NULL
+    printf("链表（尾插法）创建成功！\n");
 }
 
 //头插法建立链表C
@@ -52,7 +74,7 @@ void headCreatList(LNode *&C, int a[], int n)
     LNode *s;                               //s用来指向新申请的节点
     int i;
     C = (LNode *)malloc(sizeof(LNode));
-    C->next = NULL;                         //空
+    C->next = nullptr;                         //空
     for (i = 0; i < n; ++i) {
         s = (LNode *)malloc(sizeof(LNode));
         s->data = a[i];
@@ -60,6 +82,7 @@ void headCreatList(LNode *&C, int a[], int n)
         s->next = C->next;                  //现在s所指向的新的节点的指针域也指向C指向的节点
         C->next = s;                        //C所指向的节点的指针域使命完成，将指针s与原先的C所指向的节点的指针域连接
     }
+    printf("链表（头插法）创建成功！\n");
 
 }
 
@@ -69,7 +92,7 @@ int findAndDelElem(LNode *C, int x)
     LNode *p,*q;
     p = C;
     /*查找部分开始*/
-    while(p->next!=NULL){
+    while(p->next!= nullptr){
         if(p->next->data==x) {
             break;
         }
@@ -77,7 +100,7 @@ int findAndDelElem(LNode *C, int x)
 
     }
     /*查找部分结束*/
-    if(p->next==NULL)
+    if(p->next== nullptr)
         return 0;
     else
     {
@@ -90,32 +113,27 @@ int findAndDelElem(LNode *C, int x)
     }
 }
 
-int main()
-{
+int main() {
     LNode *C;
     LNode *L;
     printf("下面开始调用上面的函数\n");
-    int a[7] = {1,2,3,4,5,6,7};
-    int b[7] = {7,6,5,4,3,2,1};
+    int a[7] = {1, 2, 3, 4, 5, 6, 7};
+    int b[7] = {1, 2, 3, 4, 5, 6, 7};
     printf("(1)使用头插法建立链表C：\n");
     headCreatList(C, a, 7);
     printf("现在打印链表C：\n");
-    DispList(C);
+    DisplayList(C);
     printf("(2)使用尾插法建立链表L：\n");
     tailCreatList(L, b, 7);
     printf("现在打印链表L：\n");
-    DispList(L);
+    DisplayList(L);
     printf("(3)查找链表中是否有3这个值，有的话删除.\n");
     findAndDelElem(C, 3);
     printf("现在打印链表C：\n");
-    DispList(C);
+    DisplayList(C);
     findAndDelElem(L, 3);
     printf("现在打印链表L：\n");
-    DispList(L);
-    printf("%d%d", L->next->data,L->next->next->data);
-    printf("%d%d", C->next->data,C->next->next->data);
+    DisplayList(L);
     free(L);
     free(C);
-
-
 }
